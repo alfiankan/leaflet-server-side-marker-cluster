8e9db5f6da3e3609df540ed0ba8da68bfaf2460e
alfiankan <alfiankan19@gmail.com>
2022-04-24 05:33:14 +0000

    chore: initialize

---
+++ import.js
@@ -0,0 +1,43 @@
+const Pool = require('pg').Pool
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'postgres',
+    password: 'petaku',
+    port: 5432,
+})
+
+const fs = require('fs');
+
+const bigFile = fs.readFileSync('./1_million_location.txt', 'utf8')
+
+const rows = bigFile.split('\n')
+
+// clean up
+pool.query(`DELETE FROM public.marker_cluster;`, (error, results) => {
+    if (error) {
+        throw error
+    }
+})
+
+let total = 0
+rows.map(coordinate => {
+    const coor = coordinate.replace('\r', '').split(',')
+    pool.query(`INSERT INTO public.marker_cluster(location) VALUES (ST_GeomFromText('Point(${coor[0]} ${coor[1]})'));`, (error, results) => {
+        if (error) {
+            throw error
+        }
+        if (total % 1000 == 0) {
+            console.log(total)
+        }
+        total++
+    })
+
+})
+
+
+
+
+
+
+
